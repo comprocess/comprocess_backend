d7f4dc40ca2d18ac75bdc64009be2065fd1a0a13
chno17 <162543269+chno17@users.noreply.github.com>
2025-11-25 09:25:23 +0900

    models.py(일부)

--- comprocess/comprocessSW/models.py
+++ comprocess/comprocessSW/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Travel_Schedule(models.Model):
+    destination = models.CharField((""), max_length=50)
+    budget = models.CharField((""))
+    travel_date = models.CharField((""))
+    preferences = models.CharField((""))
+    extra = models.CharField((""))
